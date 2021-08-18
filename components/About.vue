d887d6077d10a6965f8b2683e9efd6687fcb4310
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-08-18 02:13:56 +0100

    adjusting aesthetics

--- components/About.vue
+++ components/About.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="px-5 has-background-grey-light" style="border-radius: 0 0 30px 30px;">
+    <div class="px-5 has-background-grey-darker" style="border-radius: 0 0 30px 30px;">
         <figure class="image is-128x128" style="margin: -20% 30% 0 30%;">
             <img class="is-rounded" src="~/assets/images/dp_pg.png">
         </figure>
@@ -7,20 +7,20 @@
             Boluwaji AKINSEFUNMI <br />
             <span class="tag is-primary mb-5">Developer</span>
         </h3>
-        <p class="subtitle has-text-black">
-            <span class="has-text-grey-darker">Phone:</span><br />
+        <p class="subtitle has-text-grey-light">
+            <span class="has-text-grey">Phone:</span><br />
             +234 810 341 2973
         </p>
-        <p class="subtitle has-text-black">
-            <span class="has-text-grey-darker">Email:</span><br />
+        <p class="subtitle has-text-grey-light">
+            <span class="has-text-grey">Email:</span><br />
             bolu.akinsefunmi@gmail.com
         </p>
-        <p class="subtitle has-text-black">
-            <span class="has-text-grey-darker">Website:</span><br />
+        <p class="subtitle has-text-grey-light">
+            <span class="has-text-grey">Website:</span><br />
             https://github.com/IAmMasterCraft
         </p>
-        <p class="subtitle has-text-black">
-            <span class="has-text-grey-darker">Location:</span><br />
+        <p class="subtitle has-text-grey-light">
+            <span class="has-text-grey">Location:</span><br />
             Lagos State
         </p>
         <h4 class="title is-4 mt-6 has-text-primary">ABOUT ME</h4>
